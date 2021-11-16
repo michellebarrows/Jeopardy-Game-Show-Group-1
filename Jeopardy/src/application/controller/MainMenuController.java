dd3f022aa389cacb3ad6516135e878051dc77270
michelle <michelle.barrows@yahoo.com>
2021-11-15 22:32:41 -0600

    team creation and image folder
    Added team creation, both teams data is passed to jeopardy main game screen through setData(Team, Team) method in JeopardyController.
    
    Team names can be accessed with Team.getName()
    
    Team scores can be accessed/modified with Team.getScore() and Team.setScore(int)
    
    Also added a folder for any application images to be stored and accessed :)

--- Jeopardy/src/application/controller/MainMenuController.java
+++ Jeopardy/src/application/controller/MainMenuController.java
@@ -2,6 +2,7 @@ package application.controller;
 
 import java.io.IOException;
 
+import application.model.Team;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -9,36 +10,67 @@ import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import javafx.scene.control.TextField;
 
 public class MainMenuController {
 
     @FXML
     private AnchorPane mainPane;
+    @FXML
+    private TextField team1NameField;
+    @FXML
+    private TextField team2NameField;
+    @FXML
+    private TextField errorMessage;
     
     @FXML
-    void handlePlay(ActionEvent event) {
-		try {
-			//System.out.println(userName);
+    void handlePlay(ActionEvent event) 
+    {
+    	Team team1 = null;
+    	Team team2 = null;
+    	
+    	if((team1NameField.getText().length() > 0) && (team2NameField.getText().length() > 0))
+    	{
+    		team1 = new Team(team1NameField.getText());
+    		team2 = new Team(team2NameField.getText());
+    	}
+    	
+    	else
+    	{
+    		errorMessage.setText("Enter a valid team name for both teams!");
+    		return;
+    	}
+    	
+		try
+		{
 			FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("./application/view/JeopardyGame.fxml"));
 			mainPane = loader.load();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
+			
+			JeopardyController control = loader.getController();
+			control.setData(team1, team2);
+			
+	        Scene scene = new Scene(mainPane);
+	        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+	        window.setResizable(false);
+	        window.setScene(scene);
+	        window.show();
+		} catch (IOException e) 
+		{
+			System.out.println("Error loading FXML file:\n");
 			e.printStackTrace();
-		}// pane you are GOING TO
-        Scene scene = new Scene(mainPane);// pane you are GOING TO show
-        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();// pane you are ON
-        window.setScene(scene);
-        window.show();
-
+			System.exit(1);
+		}
     }
 
     @FXML
-    void handleQuit(ActionEvent event) {
-
+    void handleQuit(ActionEvent event) 
+    {
+    	System.exit(0);
     }
 
     @FXML
-    void handleScores(ActionEvent event) {
+    void handleScores(ActionEvent event) 
+    {
 
     }
 
