1dc71875316a018bc1f376c6e6b32da347491cab
michelle <michelle.barrows@yahoo.com>
2021-11-19 16:38:26 -0600

    Menus finished beautified
    Main menu and jeopardy game screen features all added and cleaned up. Also made it look "better" ??

--- Jeopardy/src/application/controller/JeopardyController.java
+++ Jeopardy/src/application/controller/JeopardyController.java
@@ -6,12 +6,27 @@ import application.model.Jeopardy;
 import application.model.Question;
 import application.model.Team;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
 public class JeopardyController {
 
     @FXML
     private AnchorPane mainPane;
+    @FXML
+    private Button btn00, btn01, btn02, btn03, btn04,
+    			   btn10, btn11, btn12, btn13, btn14,
+    			   btn20, btn21, btn22, btn23, btn24,
+    			   btn30, btn31, btn32, btn33, btn34,
+    			   btn40, btn41, btn42, btn43, btn44;
+    @FXML
+    private TextField category0, category1, 
+    		category2, category3, category4;
+    @FXML
+    private TextField team1Name, team2Name;
+    @FXML
+    private TextField team1Score, team2Score;
     
     Team team1;
     Team team2;
@@ -23,12 +38,22 @@ public class JeopardyController {
     	for(Question question: Qtest) {
     		System.out.println(question);
     	}
+    	
+    	initializeButtons();
     }
     
     public void setData(Team team1, Team team2)
     {
     	this.team1 = team1;
     	this.team2 = team2;
+    	
+    	team1Name.setText(team1.getTeamName());
+    	team2Name.setText(team2.getTeamName());
+    }
+    
+    public void initializeButtons()
+    {
+    	
     }
 
 }
